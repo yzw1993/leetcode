cabe3a1122591e28aac0e3b09ee4854d4e025a51
yzw <yzw19930507@qq.com>
2017-06-08 23:53:10 +0800

    upd

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@
 public class Main {
     public static void main(String[] args){
         TwoSum ts =new TwoSum();
-        int[] nums ={2,7,12,15};
+        int[] nums ={2,7,12,15,20};
         int target = 9;
         ts.twoSum(nums,target);
-    }
+    }//22222233333
 }
