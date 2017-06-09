e8824c1637d2b053284d44fbb5fa3a024463f52c
yzw <yzw19930507@qq.com>
2017-06-09 13:50:13 +0800

    upd

--- src/AddTwoNumbers.java
+++ src/AddTwoNumbers.java
@@ -9,24 +9,26 @@ public class AddTwoNumbers {
       ListNode(int x) { val = x; }
   }
   public ListNode addTwoNumbers(ListNode l1,ListNode l2){
-         int carry =0;
-         ListNode l3 =new ListNode(0);
-         ListNode p1=l1;ListNode p2 =l2;ListNode p3 =l3;
-         while(p1 !=null||p2 !=null){
-             if(p1 !=null){
-                 carry +=p1.val;
-                 p1 =p1.next;
-             }
-             if(p2 !=null){
-                 carry +=p2.val;
-                 p2 =p2.next;
-             }
-             p3.next =new ListNode(carry%10);
-             p3 = p3.next;
-             carry /=10;
+     int sum=0;
+     ListNode l3 =new ListNode(0);
+     ListNode p1=l1;ListNode p2 =l2;ListNode p3=l3;
+     while (p1!=null||p2!=null){
+         sum/=10;
+         if(p1 !=null){
+             sum +=p1.val;
+             p1=p1.next;
          }
-         if (carry ==1)
-             p3.next=new ListNode(1);
-         return l3.next;
+         if(p2 !=null){
+             sum +=p2.val;
+             p2=p2.next;
+         }
+         p3.next=new ListNode(sum%10);
+         p3=p3.next;
+     }
+     if (sum/10==1){
+         p3.next=new ListNode(1);
+     }
+    return l3.next;
+
   }
 }
