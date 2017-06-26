5165beee9ee67d0fe0a70239745e8cf172c6a294
yzw <yzw19930507@qq.com>
2017-06-26 16:59:28 +0800

    reverse

--- src/ReverseInteger.java
+++ src/ReverseInteger.java
@@ -2,5 +2,17 @@
  * Created by zeweiyang on 2017/6/26.
  */
 public class ReverseInteger {
-    public int reverse(int x)
+    public int reverse(int x){
+        int result =0;
+        while(x !=0){
+            int tail =x%10;
+            int newResult=result*10+tail;
+            if ((newResult-tail)/10!=result){
+                return 0;
+            }
+            result =newResult;
+            x=x/10;
+        }
+        return result;
+    }
 }
