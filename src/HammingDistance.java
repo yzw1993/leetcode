956c34d82b13d014d40858032e569a2945b96555
yzw <yzw19930507@qq.com>
2017-07-07 15:00:49 +0800

    no message

---
+++ src/HammingDistance.java
@@ -0,0 +1,12 @@
+/**
+ * Created by zeweiyang on 2017/7/7.
+ */
+public class HammingDistance {
+    public  int hammingDistance(int x,int y){
+        int xor = x^y,count=0;
+        for (int i =0;i<32;i++){
+            count +=(xor>>i)&1;
+        }
+        return  count;
+    }
+}
---
+++ src/MergeTwoBinaryTrees.java
@@ -0,0 +1,30 @@
+/**
+ * Created by zeweiyang on 2017/7/7.
+ */
+public class MergeTwoBinaryTrees {
+    public class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x){
+            val =x;
+        }
+    }
+    public  TreeNode mergeTrees(TreeNode t1,TreeNode t2){
+        if (t1==null&&t2==null){
+            return null;
+        }
+        if (t1==null&&t2!=null){
+            return t2;
+        }
+        if (t1!=null&&t2==null){
+            return t1;
+        }
+        if (t1!=null&&t2!=null){
+            t1.val +=t2.val;
+            t1.left=mergeTrees(t1.left,t2.left);
+            t1.right=mergeTrees(t1.right,t2.right);
+        }
+        return t1;
+    }
+}
